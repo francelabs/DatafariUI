c08f8d32094b568071970a499c5cd078286d5c58
Romaric Pighetti <romaric.pighetti@francelabs.com>
2020-06-30 14:54:54 +0200

    Added i18next and associated tools

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useTranslation, Trans } from 'react-i18next';
 
-function App() {
+function MyComponent() {
+  const { t } = useTranslation();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Trans t={t} i18nKey="editSource">
+            Edit <code>src/App.js</code> and save to reload.
+          </Trans>
         </p>
         <a
           className="App-link"
@@ -16,10 +20,18 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {t('Learn React')}
         </a>
       </header>
     </div>
+  );
+}
+
+function App() {
+  return (
+    <Suspense fallback="loading">
+      <MyComponent />
+    </Suspense>
   );
 }
 
