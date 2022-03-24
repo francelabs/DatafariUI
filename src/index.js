59d262f1485055754123a6925877fdf3755b4c19
Romaric Pighetti <romaric.pighetti@francelabs.com>
2022-03-24 17:23:15 +0100

    Restored correct config for datafariBaseURL

--- src/index.js
+++ src/index.js
@@ -1,23 +1,20 @@
-import "react-app-polyfill/ie9";
-import "react-app-polyfill/stable";
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/stable';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import "./i18n";
+import './i18n';
 
-window.datafariBaseURL = new URL(
-  "https://datafaritest.datafari.com/Datafari",
-  window.location.href
-);
+window.datafariBaseURL = new URL('/Datafari', window.location.href);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
