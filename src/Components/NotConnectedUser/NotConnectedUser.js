5b82dc8f0e8953af8ea6a3dde257be0a797f0662
GrafSoul <grafsoul@gmail.com>
2023-12-01 13:39:36 +0500

    feat: Updated NotConnectedUser component (user's offline status).

--- src/Components/NotConnectedUser/NotConnectedUser.js
+++ src/Components/NotConnectedUser/NotConnectedUser.js
@@ -2,47 +2,47 @@
 import React, { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 import { UserContext } from '../../Contexts/user-context';
+import { APIEndpointsContext } from '../../Contexts/api-endpoints-context';
+import { QueryContext } from '../../Contexts/query-context';
 
 //** Material UI */
-import {makeStyles, Typography} from '@material-ui/core';
+import {makeStyles, Typography, Link} from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
     facetHeader: {
         margin: theme.spacing(4),
-      
-
+        display: 'flex',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
     },
     facetTitleText: {
-        fontStyle: 'normal',
-        fontWeight: 'bold', 
+        display: 'block',
+        fontStyle: 'italic',
         fontSize: '18px',
-        color: '#FF0000',
-        wordBreak: 'break-all',
-       
-    }
+        lineHeight: '1',
+        color: '#5C5C5C', 
+        paddingRight: theme.spacing(2), 
+        marginRight: theme.spacing(2),    
+        borderRight: '1px solid #B6B6B6',  
+    },
 }));
-
 
 const NotConnectedUser = ({display = false}) => {
 const classes = useStyles();
 const { t } = useTranslation();
 const { state: userState } = useContext(UserContext);
+const { apiEndpointsContext } = useContext(APIEndpointsContext);
 
-console.log(display)
-console.log(userState)
-
-if((userState.user === null) && display ){
-    return (
-        <div className={classes.facetHeader}>
-            <Typography className={classes.facetTitleText}>
-                {t('Search is in disconnected mode')}
-            </Typography>      
-        </div>
-    );
-} else {
-    return <></>;
-}
-
+    if((userState.user === null) && display ){
+        return (
+            <div className={classes.facetHeader}>
+                <Typography className={classes.facetTitleText}>{t('You are currently offline.')} </Typography>
+                <Link color="secondary" href={`${apiEndpointsContext.authURL.href}?callback=https://localhost/datafariui`}> {t('Login here to find reserved content')}</Link>    
+            </div>
+        );
+    } else {
+        return <></>;
+    }
 };
 
 export default NotConnectedUser;
